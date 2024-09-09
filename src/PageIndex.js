70814032a986d0655ac9e8936028e165b20c215c
DictumMortuum <theio.vrefos@gmail.com>
2024-09-09 19:42:43 +0300

    changes

--- src/PageIndex.js
+++ src/PageIndex.js
@@ -43,26 +43,26 @@ const Main = () => {
           <div class="col-md-12">
             <ListItem>
               <ListItemAvatar>
-                <Avatar src="assets/svg/1.svg" />
+                <Avatar src="assets/svg/1.svg" sx={{ height: 120, width: 120, marginRight: 1 }} />
               </ListItemAvatar>
-              <p class="lead ">Στους χώρους υγειονομικού ενδιαφέροντος όπως και στο ιατρείου συστήνεται η χρήση προστατευτικής μάσκας προς αποφυγή διάδοσης τυχόν λοιμώξεων μεταξύ των ασθενών.</p>
+              <p class="lead ">Στους χώρους υγειονομικού ενδιαφέροντος όπως και στο ιατρείο συστήνεται η χρήση προστατευτικής μάσκας προς αποφυγή διάδοσης τυχόν λοιμώξεων μεταξύ των ασθενών.</p>
+            </ListItem>
+          </div>
+
+          <div class="col-md-12">
+            <ListItem>
+              <ListItemAvatar >
+                <Avatar src="assets/svg/2.svg" sx={{ height: 120, width: 120, marginRight: 1 }} />
+              </ListItemAvatar>
+              <p class="lead ">Για την αποφυγή συνωστισμού και την μείωση της διασπορά λοιμώξεων συστήνεται η προσέλευση των επισκεπτών μόνο με ένα άτομο συνοδεία.</p>
             </ListItem>
           </div>
 
           <div class="col-md-12">
             <ListItem>
               <ListItemAvatar>
-                <Avatar src="assets/svg/2.svg" />
-              </ListItemAvatar>
-              <p class="lead ">Ένας συνοδός ανά εξεταζόμενο.</p>
-            </ListItem>
-          </div>
-
-          <div class="col-md-12">
-            <ListItem>
-              <ListItemAvatar>
-                <Avatar>
-                  <NotificationsIcon />
+                <Avatar sx={{ height: 120, width: 120, marginRight: 1 }}>
+                  <NotificationsIcon sx={{ height: 100, width: 100 }} />
                 </Avatar>
               </ListItemAvatar>
               <p class="lead ">Στις 30 Σεπτεμβρίου, στο χώρο του ιατρείου η συνεργαζόμενη διατροφολόγος Ελευθερία Παπαχρήστου θα παρουσιάσει σύγχρονες διατροφικές οδηγίες για τα παιδιά και θα απαντήσει σε ερωτήσεις γονέων. Για δηλώσεις συμμετοχής και περισσότερες πληροφορίες οι ενδιαφερόμενοι θα πρέπει να επικοινωνήσουν με τη γραμματεία (2105789951 και 6937432103).</p>
@@ -72,8 +72,8 @@ const Main = () => {
           <div class="col-md-12">
             <ListItem>
               <ListItemAvatar>
-                <Avatar>
-                  <NotificationsIcon />
+                <Avatar sx={{ height: 120, width: 120, marginRight: 1 }}>
+                  <NotificationsIcon sx={{ height: 100, width: 100 }} />
                 </Avatar>
               </ListItemAvatar>
               <p class="lead ">Το ιατρείο θα παραμείνει κλειστό στις 25 και 28 Οκτωβρίου. Η επικοινωνία των ασθενών θα γίνεται στο κινητό της γραμματείας ή των ιατρών.</p>
--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,13 @@
 import Grid from '@mui/material/Grid';
+import { Box } from '@mui/material';
 
 const New = ({ desc, url }) => {
   return (
-    <a class="btn btn-primary btn-lg" style={{ fontSize: "1.1rem", padding: 40, width: "100%" }} href={url}>{desc}</a>
+    <a class="btn btn-primary btn-lg" style={{ textAlign: "center", fontSize: "1.1rem", padding: 40, width: "100%" }} href={url}>
+      <Box sx={{ display: "flex", alignContent: "center", justifyContent: "center" }}>
+      {desc}
+      </Box>
+    </a>
   )
 }
 
@@ -12,7 +17,10 @@ const Main = () => {
     <h2 class="section-title"><span>τελευταια νεα</span></h2>
     <Grid container spacing={1}>
       <Grid item xs={12} md={6}>
-        <New desc="οδηγιες για ασφαλεις διακοπες" url="assets/pdf/ΟΔΗΓΙΕΣ ΓΙΑ ΑΣΦΑΛΕΙΣ ΔΙΑΚΟΠΕΣ.pages" />
+        <New desc="οδηγιες για ασφαλεις διακοπες" url="assets/pdf/ΟΔΗΓΙΕΣ ΓΙΑ ΑΣΦΑΛΕΙΣ ΔΙΑΚΟΠΕΣ.pdf" />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <New desc="λογοι απαλλαγης απο τον εμβολιασμο κατα του COVID-19" url="assets/pdf/Λόγοι-απαλλαγής-από-τον-εμβολιασμό-κατά-του-κορωνοϊού-COVID.pdf" />
       </Grid>
     </Grid>
   </div>
